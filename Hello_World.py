7bfbcf8b769b8f935209e698e85270106517531a
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-09 16:55:46 +0530

    a

--- Hello_World.py
+++ Hello_World.py
@@ -4,5 +4,4 @@ if 5>2:
 #below code will generate error
 #You have to use the same number of spaces in the same block of code, otherwise Python will give you an error
 # if 5>2"
-# print(".....")
-
+# print(".....").
