d7c2c221eb0985c17f98c76787396cb2e8ceb203
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-09 16:54:44 +0530

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -2,6 +2,7 @@ if 5>2:
     print("five is graeater than 2....")
 
 #below code will generate error
+#You have to use the same number of spaces in the same block of code, otherwise Python will give you an error
 # if 5>2"
 # print(".....")
 
