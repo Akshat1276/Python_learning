9f3720dc5247946e1a275061e751faa05a9df538
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-10 12:48:08 +0530

    Update def_func.py

--- def_func.py
+++ def_func.py
@@ -1,9 +1,16 @@
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
 x = "awesome"
 
 def myfunc():
-  x = "fantastic"
   print("Python is " + x)
 
 myfunc()
-
-print("Python is " + x)
