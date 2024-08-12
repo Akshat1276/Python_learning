0788355a86df792da41825b868b564030a1240f9
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-12 23:13:42 +0530

    Update second-to-day.py

--- second-to-day.py
+++ second-to-day.py
@@ -1,9 +1,13 @@
+import math
+
 def convertor(second):
     hour = second/3600
     minute = (second%3600)/60
     second = second%3600
     sec = second % 60
-    print("The given seconds correspond to.......%i hours  %i minutes  %i seconds", hour, minute, sec)
+    # a = math.floor(hour)
+    # b = math.floor(minute)
+    print("The given seconds correspond to....   ", math.floor(hour), " hours  ", math.floor(minute)," minutes  ", math.floor(sec), " seconds")
 
 x = int(input("Enter the Seconds:  "))
 convertor(x)
