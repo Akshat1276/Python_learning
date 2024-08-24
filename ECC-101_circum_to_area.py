4b6c9c7ca7a03e6c53da8b31f12c76b01764a4bc
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-24 16:40:17 +0530

    s

---
+++ ECC-101_circum_to_area.py
@@ -0,0 +1,4 @@
+c = float(input("C IS: "))
+r = c/(6.28)
+a = r*r*3.14
+print("THE AREA OF THE CIRCLE IS :  ", a)
---
+++ nigga_version.py
@@ -0,0 +1,7 @@
+print("this is to calculate area of a circle whose circumference is given")
+circum=float(input("what is circumference of your circle: "))
+pi=3.14
+r=circum/(2*pi)
+area=pi*(r**2)
+print("radius of this circle is:",r)
+print("area of this circle is:",area)
